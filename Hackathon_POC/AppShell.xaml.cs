56176a382942f7b6214ab109fa96ac1c8c496207
John Lalumondier <john.lalumondier@ascendlearning.com>
2023-08-24 17:04:20 -0500

    Real initial commit

---
+++ Hackathon_POC/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Hackathon_POC
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Hackathon_POC/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Hackathon_POC
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
